f65380b045a36baf445d716f1f70eb3ebe2a47de
Mika-F620 <39304201+Mika-F620@users.noreply.github.com>
2022-04-01 19:53:11 +0900

    Add files via upload
    アコーディオンを実装しよう

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -31,14 +31,95 @@ const upArrows = document.querySelectorAll(".fa-angle-up");
 //     })
 //   });
 
+/* =================================================== */
+// slideUp, slideDown, slideToggle関数を定義
+/* =================================================== */
+
+// 要素をスライドしながら非表示にする関数(jQueryのslideUpと同じ)
+const slideUp = (el, duration = 300) => {
+  el.style.height = el.offsetHeight + "px";
+  el.offsetHeight;
+  el.style.transitionProperty = "height, margin, padding";
+  el.style.transitionDuration = duration + "ms";
+  el.style.transitionTimingFunction = "ease";
+  el.style.overflow = "hidden";
+  el.style.height = 0;
+  el.style.paddingTop = 0;
+  el.style.paddingBottom = 0;
+  el.style.marginTop = 0;
+  el.style.marginBottom = 0;
+  setTimeout(() => {
+    el.style.display = "none";
+    el.style.removeProperty("height");
+    el.style.removeProperty("padding-top");
+    el.style.removeProperty("padding-bottom");
+    el.style.removeProperty("margin-top");
+    el.style.removeProperty("margin-bottom");
+    el.style.removeProperty("overflow");
+    el.style.removeProperty("transition-duration");
+    el.style.removeProperty("transition-property");
+    el.style.removeProperty("transition-timing-function");
+    el.classList.remove("is-open");
+  }, duration);
+};
+
+// 要素をスライドしながら表示する関数(jQueryのslideDownと同じ)
+const slideDown = (el, duration = 300) => {
+  el.classList.add("is-open");
+  el.style.removeProperty("display");
+  let display = window.getComputedStyle(el).display;
+  if (display === "none") {
+    display = "block";
+  }
+  el.style.display = display;
+  let height = el.offsetHeight;
+  el.style.overflow = "hidden";
+  el.style.height = 0;
+  el.style.paddingTop = 0;
+  el.style.paddingBottom = 0;
+  el.style.marginTop = 0;
+  el.style.marginBottom = 0;
+  el.offsetHeight;
+  el.style.transitionProperty = "height, margin, padding";
+  el.style.transitionDuration = duration + "ms";
+  el.style.transitionTimingFunction = "ease";
+  el.style.height = height + "px";
+  el.style.removeProperty("padding-top");
+  el.style.removeProperty("padding-bottom");
+  el.style.removeProperty("margin-top");
+  el.style.removeProperty("margin-bottom");
+  setTimeout(() => {
+    el.style.removeProperty("height");
+    el.style.removeProperty("overflow");
+    el.style.removeProperty("transition-duration");
+    el.style.removeProperty("transition-property");
+    el.style.removeProperty("transition-timing-function");
+  }, duration);
+};
+
+// 要素をスライドしながら交互に表示/非表示にする関数(jQueryのslideToggleと同じ)
+const slideToggle = (el, duration = 300) => {
+  if (window.getComputedStyle(el).display === "none") {
+    return slideDown(el, duration);
+  } else {
+    return slideUp(el, duration);
+  }
+};
+
+
   const menus =  document.querySelectorAll(".menu-title");
+  const content = document.querySelectorAll(".menu-content");
   menus.forEach((menu, index)=>{
     menu.addEventListener('click', function(){
-      menuContents[index].classList.toggle('display-none');
+      // menuContents[index].classList.toggle('display-none');
       upArrows[index].classList.toggle('display-none');
       downArrows[index].classList.toggle('display-none');
+      slideToggle(content[index]);
     })
   })
+
+
+
 
 
  
